ffea8ee1726498378294bfc3f1e27b37d4298deb
jokerrs <nemanja@joker.rs>
2019-08-05 18:39:09 +0200

    Update base64_image.php

--- base64_image.php
+++ base64_image.php
@@ -2,15 +2,15 @@
 
         /**
          * @param string $image base64 image
-         * @param array $AllowedFormat
+         * @param array $AllowedFormats
          * @param null|string $format string is 'jpg'
          * @return array|bool
          */
-        function base64_image($image, $AllowedFormat, $format = NULL){
+        function base64_image($image, $AllowedFormats, $format = NULL){
             if(preg_match('/^data:image/(\w+);base64,/', $image, $type)){
                 $image = substr($image, strpos($image, ',') + 1);
                 $type = strtolower($type[1]);
-                $AllowedExt = $AllowedFormat;
+                $AllowedExt = $AllowedFormats;
                 $return = true;
                 if(!in_array($type, $AllowedExt)){
                     $return = false;
