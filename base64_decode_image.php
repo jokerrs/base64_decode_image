4722f34773141c1a1f2cfc839591a68ca84d4e0e
jokerrs <nemanja@joker.rs>
2019-08-05 19:58:35 +0200

    Update base64_decode_image.php

--- base64_decode_image.php
+++ base64_decode_image.php
@@ -3,54 +3,54 @@
         /**
          * @param $Base64Image
          * @param array $AllowedFormats
-         * @param int|null $Format = 1; 
+         * @param int|null $Format = 1;
          * @return array|bool
          */
         function base64_decode_image($Base64Image, $AllowedFormats, $Format = NULL){
-            if(preg_match('/^data:image/(\w+);base64,/', $image, $type)){
+
+            if(preg_match('/^data:image/(\w+);base64,/', $image, $type)) {
+
                 $Base64Image = substr($Base64Image, strpos($Base64Image, ',') + 1);
-                $type = strtolower($type[1]);
-                 
-                if(in_array('jpg' ,$AllowedFormats))
-                    array_push($AllowedFormats, array ('jpeg', 'jpe', 'jif', 'jfif', 'jfi'));
-                    
-                if(in_array('svg', $AllowedFormats))
-                    array_push($AllowedFormats, array ('svgz'));
-                    
-                if(in_array('bmp', $AllowedFormats))
-                    array_push($AllowedFormats, array ('dib'));
-                    
+
+                $type = strtolower($type[ 1 ]);
+
+                if ( in_array('jpg', $AllowedFormats) )
+                    array_push($AllowedFormats, array( 'jpeg', 'jpe', 'jif', 'jfif', 'jfi' ));
+
+                if ( in_array('svg', $AllowedFormats) )
+                    array_push($AllowedFormats, array( 'svgz' ));
+
+                if ( in_array('bmp', $AllowedFormats) )
+                    array_push($AllowedFormats, array( 'dib' ));
+
                 $AllowedExt = $AllowedFormats;
+
                 $return = true;
-                if(!in_array($type, $AllowedExt))
+
+                if ( !in_array($type, $AllowedExt) )
                     $return = false;
-               
-                if($Format != NUll) {
+
+                if ( $Format != NULL ) {
                     $jpg = array( 'jpg', 'jpeg', 'jpe', 'jif', 'jfif', 'jfi' );
-                    if ( in_array($type, $jpg) ) 
+                    if ( in_array($type, $jpg) )
                         $type = 'jpg';
-                        
+
                     $svg = array( 'svg', 'svgz' );
-                    if ( in_array($type, $svg) ) {
+                    if ( in_array($type, $svg) )
                         $type = 'svg';
-                 
+
                     $bmp = array( 'bmp', 'dib' );
-                    if ( in_array($type, $bmp)  ) 
+                    if ( in_array($type, $bmp) )
                         $type = 'bmp';
-                    
                 }
 
                 $Base64Image = base64_decode($Base64Image);
-                        
-                if($Base64Image === false)
+                if ( $Base64Image === false )
                     $return = false;
-                
-                if($return) {
-                    $return = array(
-                        'file' => $Base64Image,
-                        'type' => $type
-                    );
-                }
+
+                if ( $return )
+                    $return = array( 'file' => $Base64Image, 'type' => $type );
+
             }else {
                 $return = false;
             }
